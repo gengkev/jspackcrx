f6877b8f8271b475250a77c1eebd71ef87857bcb
gengkev@gmail.com <gengkev@gmail.com@884167ee-59ac-f83e-f021-f4e235d73863>
2011-09-01 01:38:31 +0000

    Well, have some code leftover from a while ago, but I'm going to change the program flow. As always, this code is half-finished.

--- trunk/genrsa.js
+++ trunk/genrsa.js
@@ -1,4 +1,34 @@
-self.postMessage("Init'd!");
-self.onMessage=function(event) {
+
+self.postMessage({name:"loadready"});
+self.onMessage=function(evt) {
+  switch(evt.name) {
+    case "libdir":
+	  var l=evt.message;
+	  var s=["base64.min.js","jsbn.min.js","rng.min.js","rsa.min.js","sha1.min.js","rsa-sign.min.js"];
+	  for (var i=0;i<s.length;i++) {
+	    importScripts(l+s[i]);
+	  }
+	  rng_seed_time();
+	  postMessage({name:"processready"});
+	  break;
+	case "genrsadetails":
+	  rsakeygen(evt.message);
+	  break;
+	default:
+	  break;
+  }
 }
-// libs/base64.js libs/jsbn.js libs/rng.js libs.rsa.js libs/sha1.js libs/sha256.js libs/rsa-sign.js
+
+function rsakeygen(pemFile) {
+  /*
+  Do one of two things:
+  If we have a .pem file, read it and use it to find modulus and sign.
+  
+  Otherwise, make a private key and continue from there.
+  
+  */
+  var rsa=new RSAKey();
+  if (typeof pemFile=="undefined") {
+    //we are going to generate one!
+  }
+}
