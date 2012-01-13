33c5e05ace6a32d4239c540fc0b35d3a8fd8c2f5
gengkev@gmail.com <gengkev@gmail.com@884167ee-59ac-f83e-f021-f4e235d73863>
2012-01-13 03:33:02 +0000

    Messing around? Redesigning the API again.

--- jspackcrx.js
+++ jspackcrx.js
@@ -5,17 +5,16 @@
  */
 
 
+(function(){
+if (!window.Worker) {
+  throw new Error("Web Workers are not supported");
+  return;
+}
 
-
-// ok so first we need to load scripts.js
-//Options
-var libdir="/libs/"; //either specify undefined to get from code hosting or a directory to obtain scripts
-//end options
-
-if (!JSZip) throw new Error("Need JSZip!");
+var libdir = "/libs/";
 
 if (typeof libdir=="undefined") libdir=(location.protocol=="https")?"https":"http"+"://jspackcrx.googlecode.com/svn/trunk/libs/";
-else if (libdir.test(new RegExp("/$"))==false) libdir+="/";
+else if (!libdir.test(///$/)) libdir+="/";
 
 //load script.js
 (function(d,s){var t=d.createElement(s);f=d.getElementsByTagName(s)[0];t.async=!!t.type='text/javascript';
@@ -27,13 +26,68 @@ else if (libdir.test(new RegExp("/$"))==false) libdir+="/";
 })(document,'script');
 
 
+function JSCrx() {
+  this.zip={};
+  this.zip.string="";
+
+  this.privateKey={};
+  this.privateKey.string="";
+  // this.privateKey.pem="";
+
+  this.publicKey={};
+  this.publicKey.modulus="";
+  this.publicKey.exponent=0;
+  this.publicKey.der="";
+
+  this.sign={};
+  this.sign.string="";
+  this.sign.der="";
+
+  this.worker = new Worker("worker.js");
+  this.worker.postMessage({name:"Hello World!"});
+
+  this.worker.onmessage=function(e) {
+    switch(e.name) {
+      case "World Hello!":
+        break;
+      case "generateRSAKey":
+        this.publicKey.modulus = e.modulus;
+        break;
+      default:
+        break;
+  }
+}
+JSCrx.prototype.generateRSAKey = function(exponent) {
+  if (exponent!==65537) throw new Error("Exponent not supported");
+  
+  this.worker.postMessage({name:"generateRSAKey",exponent:65537});
+}
+JSCrx.prototype.formatRSAKey = function() {
+  if (!this.publicKey.modulus) throw new Error("Modulus must be generated!");
+
+  this.worker.postMessage({name:"formatRSAKey",modulus:this.publicKey.modulus});
+}
+JSCrx.prototype.addZip = function(zipString,encoding) {
+  var rawZip="";
+
+  switch(encoding) {
+    case "hex":
+      break;
+    case "base64":
+      break;
+    case "raw":
+    default:
+      rawZip = zipString;
+      break;
+  }
+
+  this.zip.string = rawZip;
+}
+JSCrx.prototype.signZip = function() {
+  if (!this.private
 
 JSZip.prototype.generateCRX=function(asBytes,zipFile,pemFile,callback)
   //create RSA key, sign, append header, callback
-  if (!window.Worker) {
-    this.onerror("Web Workers are not supported!");
-    return;
-  }
   var worker=new Worker("genrsa.js");
   worker.onmessage=function(evt){
   switch(evt.name) {
@@ -41,7 +95,7 @@ JSZip.prototype.generateCRX=function(asBytes,zipFile,pemFile,callback)
 	  worker.postMessage({name:"libdir",message:libdir});
 	  break;
 	case "processready":
-	  worker.postMessage({name:"genrsadetails",message:{
+	  worker.postMessage({name:"generateRSAKey",message:{
 	    message:pemFile
 	  }});
 	  break;
@@ -53,3 +107,5 @@ JSZip.prototype.generateCRX=function(asBytes,zipFile,pemFile,callback)
   }
   
 }
+
+});
